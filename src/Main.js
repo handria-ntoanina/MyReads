45a6a9a01b6567367d26b0e709e78f5ddafd2ffd
Herimiaina ANDRIA-NTOANINA <herimiaina@yahoo.ca>
2020-12-23 00:24:45 +0300

    chore: Main and SearchPage only Made a backup of App.js and App.css to be able to start from scratch. It is to prepare the route between Main and SearchPage and also their managed states

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React, {
+  Component
+} from 'react';
+
+class Main extends Component {
+  render() {
+    return ( <div>
+        <div class='top-bar'><h1 class='title'>My Reads</h1></div>
+      Main</div> )
+  }
+}
+
+export default Main;
---
+++ src/search/SearchPage.js
@@ -0,0 +1,11 @@
+import React, {
+  Component
+} from 'react';
+
+class SearchPage extends Component {
+  render() {
+    return ( < div > Search < /div>)
+    }
+  }
+
+export default SearchPage;
