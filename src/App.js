b3eec87da6eaa3efed64b86a503bfee33b01f5e0
Herimiaina ANDRIA-NTOANINA <herimiaina@yahoo.ca>
2020-12-23 10:28:43 +0300

    feat: Routing between search and Main The routing between search and main is now implemented including the necessary CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Main from './Main';
 import SearchPage from './search/SearchPage';
+import { Route } from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
 
 class BooksApp extends Component {
   render() {
-    return (<Main />)
+    return (
+        <div>
+          <Route exact path='/' component={Main}/>
+          <Route path='/search' component={SearchPage}/>
+        </div>
+      )
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,17 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Main extends Component {
   render() {
-    return ( <div>
-        <div class='top-bar'><h1 class='title'>My Reads</h1></div>
-      Main</div> )
+    return (<div>
+      <div class='top-bar'>
+        <h1 class='title'>My Reads</h1>
+      </div>
+      Main
+      <div className="open-search">
+        <Link className='link' to="/search">Add</Link>
+      </div>
+    </div>)
   }
 }
 
